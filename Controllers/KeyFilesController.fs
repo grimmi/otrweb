512a4ab064d224b6138c31543700bbd69c8b03bc
Arne Lehmann <arne.lehmann@gmx.de>
2017-07-17 18:44:16 +0200

    otroptions all wired up

--- Controllers/KeyFilesController.fs
+++ Controllers/KeyFilesController.fs
@@ -1,3 +1,16 @@
 namespace OtrWeb.Controllers
 
 open OtrWeb
+open Newtonsoft.Json.Linq
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Options
+
+[<Route("api/keyfiles")>]
+type KeyFilesController(otrConfig : IOptions<OtrOptions>) =
+    inherit Controller()
+
+    let options = otrConfig.Value
+
+    [<HttpGet>]
+    member this.Get() =
+        ""
--- Startup.fs
+++ Startup.fs
@@ -28,6 +28,7 @@ type Startup private () =
     // This method gets called by the runtime. Use this method to add services to the container.
     member this.ConfigureServices(services: IServiceCollection) =
         // Add framework services.
+        services.AddSingleton(this.Configuration) |> ignore
         services.Configure<OtrOptions>(this.Configuration.GetSection("Otr")) |> ignore
         services.AddMvc() |> ignore
 
