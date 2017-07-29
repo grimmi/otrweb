00bbcc9385504bbd49a2d68f58e4097396254278
Arne Lehmann <arne.lehmann@gmx.de>
2017-07-29 11:33:00 +0200

    added popup for editing episodes

--- wwwroot/js/my.js
+++ wwwroot/js/my.js
@@ -1,7 +1,20 @@
+var loadedepisodes = [];
+var currentepisode = {};
+var loadedmovies = [];
+var currentmovie = {};
+
 function pad(number){
     var padded = number + "";
     if(padded.length === 1){
         padded = "0" + padded;
     }
     return padded;
+}
+
+function filledit(index){
+    var editepisode = loadedepisodes[index];
+    document.getElementById("editshow").value = editepisode["show"];
+    document.getElementById("editseason").value = editepisode["season"];
+    document.getElementById("editname").value = editepisode["name"];
+    document.getElementById("editnumber").value = editepisode["number"];
 }
